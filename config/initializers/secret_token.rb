b723155c689bcdcc358975e89189f2ce7db3ac43
Rodrigo Dutra <dutraok@gmail.com>
2015-03-29 18:37:42 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RealCalendar::Application.config.secret_key_base = '1e7f7f29f8d3c4e6fb0c0a4829c7ae708f8430ebffc29772e9df760d3edd73c3e75a3b18584ebcc7d4611e3f1e90f74cfa27105d974d95cfbd6612752625fdc6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RealCalendar::Application.config.session_store :cookie_store, key: '_RealCalendar_session'
